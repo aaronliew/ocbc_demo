a914b03a606f2a1af98d1fe8ed4216ed2924ce36
Aaron Liew <aaron_liew1818@hotmail.com>
2022-01-14 22:56:46 +0800

    [REFACTOR] Handle invalid argument exception for login request

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,10 +8,12 @@ import com.example.demo.exception.InvalidArgumentException;
 import com.example.demo.service.TransactionService;
 import com.example.demo.util.ValidationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("auth")
@@ -28,12 +30,16 @@ public class AuthController {
 
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest loginRequest){
-        validateLoginRequest(loginRequest);
-        loginRequest.setUsername(loginRequest.getUsername().toLowerCase());
-        LoginResponse loginResponse = authService.login(loginRequest);
-        Debt debt = transactionService.getDebtSummary(loginResponse.getId());
-        loginResponse.setDebt(debt);
-        return loginResponse;
+        try {
+            validateLoginRequest(loginRequest);
+            loginRequest.setUsername(loginRequest.getUsername().toLowerCase());
+            LoginResponse loginResponse = authService.login(loginRequest);
+            Debt debt = transactionService.getDebtSummary(loginResponse.getId());
+            loginResponse.setDebt(debt);
+            return loginResponse;
+        } catch (InvalidArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid argument");
+        }
     }
 
     public void validateLoginRequest(LoginRequest loginRequest) throws InvalidArgumentException {
